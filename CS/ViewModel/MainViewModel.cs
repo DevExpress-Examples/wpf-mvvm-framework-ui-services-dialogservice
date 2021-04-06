adb7c087304967a83fa6531e62e5d5f669e1906c
Eugene <eugeniy.lapshov@devexpress.com>
2021-04-06 16:12:06 +0300

    Update MainViewModel.cs

--- CS/ViewModel/MainViewModel.cs
+++ CS/ViewModel/MainViewModel.cs
@@ -1,46 +1,59 @@
-﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm;
 using DevExpress.Mvvm.POCO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
 
 namespace Example.ViewModel {
-public class MainViewModel {
-    protected IDialogService DialogService { get { return this.GetService<IDialogService>(); } }
-    RegistrationViewModel registrationViewModel = null;
-    public void ShowRegistrationForm() {
-        if(registrationViewModel == null)
-            registrationViewModel = new RegistrationViewModel();
+    public class MainViewModel {
+        protected IDialogService DialogService { get { return this.GetService<IDialogService>(); } }
+        RegistrationViewModel registrationViewModel = null;
+        public void ShowRegistrationForm() {
+            if(registrationViewModel == null)
+                registrationViewModel = new RegistrationViewModel();
 
-        UICommand registerCommand = new UICommand() {
-            Caption = "Register",
-            IsCancel = false,
-            IsDefault = true,
-            Command = new DelegateCommand<CancelEventArgs>(
-                x => myExecuteMethod(),
-                x => !string.IsNullOrEmpty(registrationViewModel.UserName)),
-        };
+            UICommand registerCommand = new UICommand(
+                id: null,
+                caption: "Register",
+                command: new DelegateCommand<CancelEventArgs>(
+                    cancelArgs => {
+                        try {
+                            myExecuteMethod();
+                        }
+                        catch (Exception e) {
+                            this.GetService<IMessageBoxService>().ShowMessage(e.Message, "Error", MessageButton.OK);
+                            cancelArgs.Cancel = true;
+                        }
+                    },
+                    cancelArgs => !string.IsNullOrEmpty(registrationViewModel.UserName)
+                ),
+                isDefault: true,
+                isCancel: false
+            );
         
-        UICommand cancelCommand = new UICommand() {
-            Id = MessageBoxResult.Cancel,
-            Caption = "Cancel",
-            IsCancel = true,
-            IsDefault = false,
-        };
+            UICommand cancelCommand = new UICommand(
+                id: MessageBoxResult.Cancel,
+                caption: "Cancel",
+                command: null,
+                isDefault: false,
+                isCancel: true
+            );
 
-        UICommand result = DialogService.ShowDialog(
-            dialogCommands: new List<UICommand>() { registerCommand, cancelCommand },
-            title: "Registration Dialog",
-            viewModel: registrationViewModel);
+            UICommand result = DialogService.ShowDialog(
+                dialogCommands: new List<UICommand>() { registerCommand, cancelCommand },
+                title: "Registration Dialog",
+                viewModel: registrationViewModel
+            );
 
-        if (result == registerCommand) {
-            //...
+            if (result == registerCommand) {
+                //...
+            }
+        }
+
+        private void myExecuteMethod() {
+            Debug.Print("Registration complete");
         }
     }
-
-    private void myExecuteMethod() {
-        Debug.Print("Registration complete");
-    }
-}
 }
