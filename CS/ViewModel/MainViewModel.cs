445a09f7d4cb63ef5bc23f29b0023076caac9f33
AndreySlabov <andrey.slabov@devexpress.com>
2022-09-26 18:24:36 +0400

    Add Async Command

--- CS/ViewModel/MainViewModel.cs
+++ CS/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using DevExpress.Mvvm.DataAnnotations;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Example.ViewModel {
@@ -18,6 +19,26 @@ namespace Example.ViewModel {
 
         [Command]
         public void ShowRegistrationForm() {
+            UICommand registerAsyncCommand = new UICommand(
+               id: null,
+               caption: "Register Async",
+               command: new AsyncCommand<CancelEventArgs>(
+                   async cancelArgs => {
+                       try {
+                           await MyAsyncExecuteMethod();
+                       }
+                       catch (Exception e) {
+                           MessageBoxService.ShowMessage(e.Message, "Error", MessageButton.OK);
+                           cancelArgs.Cancel = true;
+                       }
+                   },
+                   cancelArgs => !string.IsNullOrEmpty(registrationViewModel.UserName)
+               ),
+               asyncDisplayMode: AsyncDisplayMode.Wait,
+               isDefault: false,
+               isCancel: false
+           );
+
             UICommand registerCommand = new UICommand(
                 id: null,
                 caption: "Register",
@@ -31,12 +52,12 @@ namespace Example.ViewModel {
                             cancelArgs.Cancel = true;
                         }
                     },
-                    cancelArgs => !string.IsNullOrEmpty(registrationViewModel.UserName)
+                    cancelArgs => !string.IsNullOrEmpty(registrationViewModel.UserName) && !((IAsyncCommand)registerAsyncCommand.Command).IsExecuting
                 ),
                 isDefault: true,
                 isCancel: false
             );
-        
+
             UICommand cancelCommand = new UICommand(
                 id: MessageBoxResult.Cancel,
                 caption: "Cancel",
@@ -46,17 +67,22 @@ namespace Example.ViewModel {
             );
 
             UICommand result = DialogService.ShowDialog(
-                dialogCommands: new List<UICommand>() { registerCommand, cancelCommand },
+                dialogCommands: new List<UICommand>() { registerAsyncCommand, registerCommand, cancelCommand },
                 title: "Registration Dialog",
                 viewModel: registrationViewModel
             );
 
-            if(result == registerCommand) {
+            if (result == registerCommand) {
                 // ...
             }
         }
 
         void MyExecuteMethod() {
+            // ...
+        }
+
+        async Task MyAsyncExecuteMethod() {
+            await Task.Delay(2000);
             // ...
         }
     }
